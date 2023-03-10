a5cc38e17fcb4747cb0013ab501ab6de2a966884
Sad-polar-bear <942583449@qq.com>
2023-03-10 18:48:53 +0800

    Update BaseRpcServer.java

--- core/src/main/java/com/alibaba/nacos/core/remote/BaseRpcServer.java
+++ core/src/main/java/com/alibaba/nacos/core/remote/BaseRpcServer.java
@@ -39,7 +39,7 @@ public abstract class BaseRpcServer {
     /**
      * Start sever.
      */
-    // 在Bean注解初始化完之后装载该类方法。用于启动gRPC服务端一元调用、流式调用服务端口监听
+    // 在Bean注解初始化完之后装载该类方法。用于启动gRPC服务端一元调用、流式调用服务端口监听（包含gRPC路由的初始化功能）
     @PostConstruct
     public void start() throws Exception {
         String serverName = getClass().getSimpleName();
